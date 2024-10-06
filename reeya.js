5448476e61086dab3515bfd9fd65c1a1df77414d
reeyabasnet <reeyabasnet555@gmail.com>
2024-10-06 02:06:07 -0700

    haha

--- reeya.js
+++ reeya.js
@@ -5,3 +5,13 @@ const manishisgay=true
 console.log(firstname)
 console.log("manish")
 console.log(age)
+
+const haha='momo' 
+const hehe="samosa"
+const huhu="chicken chilly momo"
+
+const hululu = ['momo','samosa','chicken momo']
+console.log(hululu[0])
+
+const hahaha=['reeya','heeya','seeya']
+console.log(hahaha[0])
