6464a5da0243e85d7d2e17b914a34c02b171bc48
reeyabasnet <reeyabasnet555@gmail.com>
2024-10-07 00:50:46 -0700

    learn about object

--- reeya.js
+++ reeya.js
@@ -15,3 +15,55 @@ console.log(hululu[0])
 
 const hahaha=['reeya','heeya','seeya']
 console.log(hahaha[0])
+const friends = ["golu","molu","holu"]
+
+// const name = "Manish Basnet"
+// const age = 22 
+// const addreess = "Itahari"
+
+const me = {
+    name : "Manish Basnet", 
+    age : 22, 
+    address : "Itahari"
+}
+console.log(me.name)
+console.log(me.address)
+
+const aboutme={
+    name:"Reeya Basnet",
+    age:21,
+    address:["ktm",'brt',{
+        huhu : 'dharan'
+    }]
+    
+
+
+}
+console.log(aboutme.name)
+console.log(aboutme.age)
+console.log(aboutme.address)
+
+ 
+console.log(aboutme.address[2])
+console.log(aboutme.address[2].huhu) 
+
+const huhus = [
+{
+    name : "reeya"
+}
+]
+console.log(huhus[0].name)
+const lulu = [{name:'reeya'},{name:"manish"}]
+console.log(lulu[1].name)
+
+const objectHuhu={
+    name : "haha"
+}
+objectHuhu.age = 22
+console.log(objectHuhu)
+
+const chowmein = {
+    name : "chowmein"
+}
+chowmein.price = 200
+console.log(chowmein)
